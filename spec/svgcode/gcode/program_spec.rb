6a43d60f5bf7dafa927fa6e02136869ec97af86b
Ben Williams <8enwilliams@gmail.com>
2017-11-25 23:42:33 +1100

    spec'd gcode program.new

--- spec/svgcode/gcode/program_spec.rb
+++ spec/svgcode/gcode/program_spec.rb
@@ -1,8 +1,38 @@
 require 'spec_helper'
 
 RSpec.describe Svgcode::GCode::Program do
-  skip '.new' do
+  describe '.new' do
+    context 'when a feedrate of 2.3, a clearance of 28.5, a depth of 18 and a \
+    command of G01 X18.99 Y52.222 is passed' do
+      let(:program) do
+        Svgcode::GCode::Program.new(
+          feedrate: 2.3,
+          clearance: 28.5,
+          depth: 18,
+          commands: [
+            Svgcode::GCode::Command.new('G01 X18.99 Y52.222')
+          ]
+        )
+      end
 
+      it 'sets the feedrate option' do
+        expect(program.opts[:feedrate]).to eq 2.3
+      end
+
+      it 'sets the clearance option' do
+        expect(program.opts[:clearance]).to eq 28.5
+      end
+
+      it 'sets the depth option' do
+        expect(program.opts[:depth]).to eq 18
+      end
+
+      it 'sets the commands' do
+        expect(program.commands).to eq [
+          Svgcode::GCode::Command.new('G01 X18.99 Y52.222')
+        ]
+      end
+    end
   end
 
   skip '#feedrate' do
